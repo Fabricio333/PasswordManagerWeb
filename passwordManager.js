62d29494a55a854309048f1764655bec362c8b9c
Fabricio333 <acostafabricio777@gmail.com>
2025-01-12 21:59:26 -0300

    Sync

--- passwordManager.js
+++ passwordManager.js
@@ -356,6 +356,26 @@ function generateValidMnemonic() {
     // Example usage generateValidMnemonic().then(mnemonic => console.log("Generated Mnemonic:", mnemonic)).catch(console.error);
 
 }
+// Flag variable to keep track of whether settings are visible or not
+let isVisible = false;
+
+/**
+ * Toggles visibility of settings container.
+ */
+function toggleSettings() {
+    // Get reference to #toggle-container element
+    const container = document.getElementById('toggle-container');
+
+    // If settings are currently visible, hide them; otherwise show them
+    if (isVisible) {
+        container.style.display = 'none';
+    } else {
+        container.style.display = 'block';
+    }
+
+    // Toggle the flag variable to remember new state
+    isVisible = !isVisible;
+}
 
 // Check inputs with common sites list
 function checkSiteInput(){}
